b9d849f2c384434e34975710d5ae67411979095b
JorgeOchoaTobar <jorge8a_112@hotmail.com>
2017-03-05 01:16:46 -0600

    Practica2-Primera
    Primer programa

---
+++ Practica2_201314795/src/practica2_201314795/Practica2_201314795.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica2_201314795;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Practica2_201314795 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal bienvenidos = new Principal();
+        bienvenidos.setVisible(true);
+        bienvenidos.setLocationRelativeTo(null);
+    }
+    
+}
